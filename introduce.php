20aff8846473f9ce3ada5941000980f8bc537c59
64309010019 <64309010019@chontech.com>
2022-02-17 13:34:26 +0700

    test

---
+++ introduce.php
@@ -0,0 +1,41 @@
+<head>
+    <title>Introduce019</title>
+    <style>
+    .p2 {
+    font-family: Arial, Helvetica, sans-serif;
+    }
+    .p3 {
+    font-family: "Lucida Console", "Courier New", monospace;
+    }
+    .center {
+	border-radius: 25px; 
+    background: #33CC99;
+	margin: auto;
+	width: 50%;
+	padding: 10px;
+	text-align: center;
+    }
+    .center1 {
+	border-radius: 25px;
+    background: #33FF66;
+	margin: auto;
+	width: 80%;
+	padding: 10px;
+	text-align: center;
+    }
+    </style>
+</head>
+<body bgcolor="#99FF99">
+    <div class="center">
+	<p class="p3">Introduce Myself.</p>
+	</div><br>
+    <div class="center1" style="text-align:center">
+    <img src="noei.jpg" width="200" border="3">
+    <h1>My name is Krittiyanee Jantabandit</h1>
+    <p class="p2">I am studying at Chonburi Technical College.</p>
+    <p>แผนก : เทคโนโลยีสารสนเทศ</p>
+    <h3 class="p2">Close Friend : นางสาว ภัทรวดี นะราชรัมย์ <img src="pook.jpg" width="100" border="3" align="center"></h3>
+    
+    <h3>งานอดิเรก : ฟังเพลง เล่นฟุตบอล <img src="n1.jpg" width="100" border="3" align="center"></h3>
+    </div>
+</body>
